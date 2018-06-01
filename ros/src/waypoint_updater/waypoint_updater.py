222af6ef95bb252b59d98e63761e67efc471f229
mcounter <admin@mcounter.com.ua>
2018-06-01 14:32:17 -0700

    Initial waypoint updater ready

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -6,6 +6,8 @@ from styx_msgs.msg import Lane, Waypoint
 
 import math
 
+from scipy.spatial import KDTree
+
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
 
@@ -22,7 +24,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-
+FINAL_WP_PUBLISH_FREQ = 50 # Frequency of publishing of final waypoints
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -33,20 +35,82 @@ class WaypointUpdater(object):
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
 
-        rospy.spin()
+        self.is_initialized = False
+        self.base_waypoints = None
+        self.base_waypoints_kdtree = None
+
+        self.hero_position = None
+
+        self._main_cycle()
+
+    def _main_cycle(self):
+        rate = rospy.Rate(FINAL_WP_PUBLISH_FREQ)
+        while not rospy.is_shutdown():
+            if self.is_initialized and bool(self.hero_position):
+                next_wp_idx = self.get_next_waypoint_idx(self.hero_position.pose.position.x, self.hero_position.pose.position.y)
+                upd_lane = self.prepare_lane(next_wp_idx)
+                self.publish_lane(upd_lane)
+                
+            rate.sleep()
+
+    def publish_lane(self, lane):
+        self.final_waypoints_pub.publish(lane)
+
+    def get_next_waypoint_idx(self, x, y):
+        """
+        Return nearest next waypointfrom the list.
+        """
+
+        dist, idx = self.base_waypoints_kdtree.query([x, y])
+
+        waypoints_num = len(self.base_waypoints)
+        if waypoints_num <= 1:
+            return -1
+
+        cur_waypoint = self.base_waypoints[idx]
+        cur_dx, cur_dy = (x - cur_waypoint.pose.pose.position.x, y - cur_waypoint.pose.pose.position.y)
+
+        if (idx + 1) < waypoints_num:
+            next_waypoint = self.base_waypoints[idx + 1]
+            next_dx, next_dy = (x - next_waypoint.pose.pose.position.x, y - next_waypoint.pose.pose.position.y)
+            if (next_dx * cur_dx + next_dy * cur_dy) <= 0:
+                return idx + 1
+            else:
+                return idx
+
+        if idx >= 1:
+            prev_waypoint = self.base_waypoints[idx - 1]
+            prev_dx, prev_dy = (x - prev_waypoint.pose.pose.position.x, y - prev_waypoint.pose.pose.position.y)
+            if (prev_dx * cur_dx + prev_dy * cur_dy) < 0:
+                return idx
+
+        return -1
+
+    def prepare_lane(self, start_idx):
+        upd_lane = Lane()
+
+        if start_idx >= 0:
+            for waypoint in self.base_waypoints[start_idx:start_idx + FINAL_WP_PUBLISH_FREQ]:
+                upd_lane.waypoints.append(waypoint)
+
+        return upd_lane
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        self.hero_position = msg
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        # Normally is called once, but can be reinitialized if necessary
+        self.is_initialized = False        
+        
+        self.base_waypoints = waypoints.waypoints
+        base_waypoints_spatial = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in self.base_waypoints]
+        self.base_waypoints_kdtree = KDTree(base_waypoints_spatial)
+
+        self.is_initialized = True
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
