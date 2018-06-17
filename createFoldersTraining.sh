e8485117a3d3198db2fc4fa82f0ab9d8a0cff16f
Marius Jansen <mariusb.jansen@googlemail.com>
2018-06-17 10:54:39 -0700

    branch for training data generation on the simulator

---
+++ createFoldersTraining.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# create folders for training
+
+# folder training RED traffic lights
+mkdir -p /home/student/train/0
+# folder training YELLOW traffic lights
+mkdir -p /home/student/train/1
+# folder training GREEN traffic lights
+mkdir -p /home/student/train/2
+# folder training no traffic light visible (add manually)
+mkdir -p /home/student/train/3
+
+
--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,48 @@
 from styx_msgs.msg import TrafficLight
+import numpy as np
+import cv2
+
+#import ptvsd
+#ptvsd.enable_attach("my_secret", address = ('127.0.0.1', 3000))
+#ptvsd.wait_for_attach()
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
         pass
 
-    def get_classification(self, image):
-        """Determines the color of the traffic light in the image
+    def get_classification(self, image, light):
+        """Creates training data from the simulator and the given ground truth
+
+        Will place images in folder with name 0,1 or 2 for RED, YELLOW, GREEN
+        Manual steps necessary: 
+            - Review if there are false positives in any folder
+            - Move images with no traffic light visible in the folder 3 (UNKNOWN)
+                - For example do it with an empty recycle bin like this
+                    - Install gthumb
+                    - Open gthumb, click until you find a candidate you want to remove
+                    - Delete by pressing delete key
+                    - Find all your candidates in recycle bin
+                    - Move all those candidates to folder 3 (UNKNOWN)
+                    - done
 
         Args:
             image (cv::Mat): image containing the traffic light
 
         Returns:
+            Not necessary in this context
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+
+        root = "/home/student/train/"
+        filename = root+str(light.state)+"/"+str(self.cnt)+".png"
+        cv2.imwrite(filename,image)
+        self.cnt+=1
+        
+        # in this context UNKNOWN is OK. purpose is to create training data
+        # please see code above
         return TrafficLight.UNKNOWN
+
+
+    cnt = long(0)
