f51aad0fe1b270e9c5cc17c6b7aa71ec1b5e1d9c
Marius Jansen <mariusb.jansen@googlemail.com>
2018-06-23 06:38:12 -0700

    traffic light classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,104 @@
 from styx_msgs.msg import TrafficLight
+import rospy
+import tensorflow as tf
+import numpy as np
+import cv2
+
+# TODO fix this absolute path
+PATH_TO_CKPT = '/home/student/RealSDC/ros/src/tl_detector/light_classification/ssd_mobilenet_v1_coco_2017_11_17.pb'
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+          od_graph_def = tf.GraphDef()
+          with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+            
+        self.sess = None
+        self.setup = False
+        self.tensor_dict = None
+        self.image_tensor = None
 
     def get_classification(self, image):
-        """Determines the color of the traffic light in the image
+        
+        cvimage = image[...,::-1]
+        (im_height, im_width) = cvimage.shape[:2]
+        npimage = np.array(cvimage.reshape(im_height, im_width, 3)).astype(np.uint8)      
+        
+        # detection call
+        output = self.run_inference_for_single_image(npimage)
+        
+        # only for testing/plotting -> dont introduce in ROS node
+        CLASS_TRAFFIC_LIGHT = 10
+        THRESHOLD_SCORE = 0.90
+                
+        boxes = output['detection_boxes']
+        classes =  output['detection_classes']
+        scores = output['detection_scores']
+        
+        match = False
+        for i in range(len(boxes)):
+            if classes[i] == CLASS_TRAFFIC_LIGHT and scores[i] > THRESHOLD_SCORE:
+                
+                # extract/crop region of interest and plot
+                if match is False:
+                    rospy.loginfo("I found a traffic light")
+                    match = True
+        
+        if match is False:
+            rospy.loginfo("No traffic light")
 
-        Args:
-            image (cv::Mat): image containing the traffic light
-
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
-        """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        return None
+    
+    def run_inference_for_single_image(self, image):
+      with self.detection_graph.as_default():
+        if (self.setup == False):
+            with tf.Session() as self.sess:
+              # Get handles to input and output tensors
+              ops = tf.get_default_graph().get_operations()
+              all_tensor_names = {output.name for op in ops for output in op.outputs}
+              self.tensor_dict = {}
+              for key in [
+                  'num_detections', 'detection_boxes', 'detection_scores',
+                  'detection_classes', 'detection_masks'
+              ]:
+                tensor_name = key + ':0'
+                if tensor_name in all_tensor_names:
+                  self.tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                      tensor_name)
+              if 'detection_masks' in self.tensor_dict:
+                # The following processing is only for single image
+                detection_boxes = tf.squeeze(self.tensor_dict['detection_boxes'], [0])
+                detection_masks = tf.squeeze(self.tensor_dict['detection_masks'], [0])
+                # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+                real_num_detection = tf.cast(self.tensor_dict['num_detections'][0], tf.int32)
+                detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+                detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+                detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+                    detection_masks, detection_boxes, image.shape[0], image.shape[1])
+                detection_masks_reframed = tf.cast(
+                    tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+                # Follow the convention by adding back the batch dimension
+                self.tensor_dict['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
+              self.image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+              self.setup = True
+        
+    
+        if (self.setup == True):
+            # Run inference
+            output_dict = self.sess.run(self.tensor_dict,feed_dict={self.image_tensor: np.expand_dims(image, 0)})
+        
+            # all outputs are float32 numpy arrays, so convert types as appropriate
+            output_dict['num_detections'] = int(output_dict['num_detections'][0])
+            output_dict['detection_classes'] = output_dict[
+                'detection_classes'][0].astype(np.uint8)
+            output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+            output_dict['detection_scores'] = output_dict['detection_scores'][0]
+            if 'detection_masks' in output_dict:
+                output_dict['detection_masks'] = output_dict['detection_masks'][0]
+                return output_dict
+        
+        return output_dict
